22fb53e38a12303ac4d1e2f5379c9f4e874fe99f
andres saldaña <andysalda20@novacampo.com>
2023-05-17 16:43:09 -0500

    actulizacion04.41_17.05.23

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,4 +1,36 @@
+const crearNuevaLinea = (nombre,email) => {
+    const linea = document.createElement('tr');
+    const contenido = `<tr>
+    <td class="td" data-td>
+    ${nombre}</td>
+    <td>
+    ${email}</td>
+    <td>
+      <ul class="table__button-control">
+        <li>
+          <a
+            href="../screens/editar_cliente.html"
+            class="simple-button simple-button--edit"
+            >Editar</a
+          >
+        </li>
+        <li>
+          <button
+            class="simple-button simple-button--delete"
+            type="button"
+          >
+            Eliminar
+          </button>
+        </li>
+      </ul>
+    </td>
+  </tr>`
+  linea.innerHTML = contenido;
+  return linea
+  
+}
 const http = new XMLHttpRequest();
+//console.log(http);
 
 //Abrir http(metodo.url)
 
@@ -8,7 +40,7 @@ const http = new XMLHttpRequest();
 //UPDATE - PUT/PATCH
 //DELETE - DELETE
 
-http.open("GET", "http://localhost:3000/perfil")
+http.open("GET", "http://localhost:3000/perfil");
 http.send();
 http.onload = () => {
     const data = http.response;
